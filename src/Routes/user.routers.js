8381b936acf7345146654d28c95ff0bc248e38a2
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2025-01-07 19:17:56 +0530

    Added new routes and controller functions for user watchHistory management

--- src/Routes/user.routers.js
+++ src/Routes/user.routers.js
@@ -18,8 +18,14 @@ router.route('/register').post(
   registerUser,
 );
 router.route('/login').post(loginUser);
-//secured routes
 router.route('/logout').post(verifyJwt, logOutUser);
 router.route('/refresh-token').post(RefreshTokenResponse);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+router.route("/history").get(verifyJwt, getWatchHistory)
 
 export default router;
